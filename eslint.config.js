7102f3a5950de673c4d473e370a36dfc4a9f3ca3
suBmit52 <sumitmishra0052@gmail.com>
2025-11-22 11:21:08 +0530

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,16 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.js', '**/*.jsx'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
@@ -24,6 +28,9 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
     },
   },
 ])
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// css imports
 import './index.css'
+// components imports
 import App from './App.jsx'
+// library imports
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
-
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 
 )
