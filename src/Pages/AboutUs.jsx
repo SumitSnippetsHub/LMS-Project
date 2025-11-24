96dfbba6318486be3873ea2684be6e7fca9faea2
suBmit52 <sumitmishra0052@gmail.com>
2025-11-24 17:22:30 +0530

    fixed the carausel

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -36,41 +36,65 @@ export default function AboutUs() {
 
                 </div>
 
-                <div className="carousel w-[500px] h-[500px] mb-10 m-auto">
+                <div className="carousel w-1/2 my-16 mb-10 m-auto h-auto">
                     <div id="slide1" className="carousel-item relative w-full">
-                        <img
-                            src={apj}
-                            className="w-[500px] h-[500px]" />
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide4" className="btn btn-circle">❮</a>
-                            <a href="#slide2" className="btn btn-circle">❯</a>
+                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+                            <img
+                                src={apj}
+                                className="w-35 rounded-full border-2 border-gray-400" />
+                            <p className="text-xl text-gray-200">
+                                {"Failure will never overtake me if my determination to succeed is strong enough."}
+                            </p>
+                            <h4 className="text-2xl font-semibold">Apj Abdul Kalam</h4>
+                            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                                <a href="#slide4" className="btn btn-circle">❮</a>
+                                <a href="#slide2" className="btn btn-circle">❯</a>
+                            </div>
                         </div>
                     </div>
                     <div id="slide2" className="carousel-item relative w-full">
-                        <img
-                            src={billgates}
-                            className="w-[500px] h-[500px]" />
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide1" className="btn btn-circle">❮</a>
-                            <a href="#slide3" className="btn btn-circle">❯</a>
+                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+                            <img
+                                src={billgates}
+                                className="w-35 rounded-full border-2 border-gray-400" />
+                            <p className="text-xl text-gray-200">
+                                {"Your time is limited, so don't waste it living someone else's life"}
+                            </p>
+                            <h4 className="text-2xl font-semibold">Bill Gates</h4>
+                            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                                <a href="#slide1" className="btn btn-circle">❮</a>
+                                <a href="#slide3" className="btn btn-circle">❯</a>
+                            </div>
                         </div>
                     </div>
                     <div id="slide3" className="carousel-item relative w-full">
-                        <img
-                            src={nelson}
-                            className="w-[500px] h-[500px]" />
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide2" className="btn btn-circle">❮</a>
-                            <a href="#slide4" className="btn btn-circle">❯</a>
+                        <div className="flex flex-col items-center justify-center  gap-4 px-[15%]">
+                            <img
+                                src={nelson}
+                                className="w-35 rounded-full border-2 border-gray-400" />
+                            <p className="text-xl text-gray-200">
+                                {"Educations is the most powerful tool you can use to change the world."}
+                            </p>
+                            <h4 className="text-2xl font-semibold">Nelson Mandela</h4>
+                            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                                <a href="#slide2" className="btn btn-circle">❮</a>
+                                <a href="#slide4" className="btn btn-circle">❯</a>
+                            </div>
                         </div>
                     </div>
                     <div id="slide4" className="carousel-item relative w-full">
-                        <img
-                            src={steve}
-                            className="w-[500px] h-[500px]" />
-                        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                            <a href="#slide3" className="btn btn-circle">❮</a>
-                            <a href="#slide1" className="btn btn-circle">❯</a>
+                        <div className="flex flex-col items-center justify-center gap-4 px-[15%]">
+                            <img
+                                src={apj}
+                                className="w-35 rounded-full border-2 border-gray-400" />
+                            <p className="text-xl text-gray-200">
+                                {"It Is Very Easy To Defeat Someone, But It Is Very Hard To Win Someone"}
+                            </p>
+                            <h4 className="text-2xl font-semibold">Apj Abdul Kalam</h4>
+                            <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                                <a href="#slide3" className="btn btn-circle">❮</a>
+                                <a href="#slide1" className="btn btn-circle">❯</a>
+                            </div>
                         </div>
                     </div>
                 </div>
