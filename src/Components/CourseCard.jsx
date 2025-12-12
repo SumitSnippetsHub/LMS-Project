cd16ce8556f79ec469bef32c4686efe079ff20de
suBmit52 <sumitmishra0052@gmail.com>
2025-12-12 12:40:52 +0530

    Completed course list page flow.

--- src/Components/CourseCard.jsx
+++ src/Components/CourseCard.jsx
@@ -5,7 +5,7 @@ export default function CourseCard({ data }) {
 
     return (
         <div onClick={() => navigate("/course/description")}
-            className="text-white w-[22rem] h-[430ox] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700">
+            className="text-white w-88 h-[430ox] shadow-lg rounded-lg cursor-pointer group overflow-hidden bg-zinc-700">
             <div className="overflow-hidden">
                 <img className="h-48 w-full rounded-tl-lg rounded-tr-lg group-hover:scale-[1.2] transition-all ease-in-out duration-300"
                     src={data?.thumbnail?.secure_url} alt="course thumbnail"
